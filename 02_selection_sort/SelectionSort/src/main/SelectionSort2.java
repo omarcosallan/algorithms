15c440b23a94b6944303b6f3cddc3994aafa76dc
Marcos Allan <marcosallanm1to@gmail.com>
2025-07-04 20:18:54 -0300

    chapter 2

--- 02_selection_sort/SelectionSort/src/main/SelectionSort2.java
+++ 02_selection_sort/SelectionSort/src/main/SelectionSort2.java
@@ -1,38 +1,23 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class SelectionSort {
+public class SelectionSort2 {
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<>(Arrays.asList(5, 3, 6, 2, 10));
-        System.out.println(selectionSort(arr));
+        int[] arr = {5, 3, 6, 2, 10};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static List<Integer> selectionSort(List<Integer> arr) {
-        List<Integer> newArr = new ArrayList<>();
-
-        int size = arr.size();
-        for (int i = 0; i < size; i++) {
-            int smallest = findSmallest(arr);
-            newArr.add(arr.remove(smallest));
-        }
-
-        return newArr;
-    }
-
-    private static int findSmallest(List<Integer> arr) {
-        int smallest = arr.get(0);
-        int smallestIndex = 0;
-
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr.get(i) < smallest) {
-                smallest = arr.get(i);
-                smallestIndex = i;
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
-
-        return smallestIndex;
     }
 }
