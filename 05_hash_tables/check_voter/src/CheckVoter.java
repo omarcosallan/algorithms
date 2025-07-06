19f2fe55cc2cf596440d44f3031d5560ae48acf5
Marcos Allan <marcosallanm1to@gmail.com>
2025-07-06 08:39:07 -0300

    code for chapter 5 in java

---
+++ 05_hash_tables/check_voter/src/CheckVoter.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckVoter {
+    private static final Map<String, Boolean> voted = new HashMap<>();
+
+    public static void main(String[] args) {
+        checkVoter("tom");
+        checkVoter("mike");
+        checkVoter("mike");
+    }
+
+    private static void checkVoter(String name) {
+        if (voted.containsKey(name)) {
+            System.out.println("kick them out!");
+        } else {
+            voted.put(name, true);
+            System.out.println("let them vote!");
+        }
+    }
+}
